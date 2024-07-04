495eb3b5155555947db5c8f678dcd478f5ca9aff
Adrian <122298418+Adrian-Costa@users.noreply.github.com>
2024-07-04 15:52:16 -0400

    page button add

--- src/components/ProjectsCards/index.jsx
+++ src/components/ProjectsCards/index.jsx
@@ -1,13 +1,14 @@
 
 import style from "../ProjectsCards/ProjectsCard.style.module.scss"
 
-export const ProjectsCard = ({title, description, link, src, alt}) =>{
+export const ProjectsCard = ({title, description, linkRepo, linkPage, src, alt}) =>{
     return(
         <div className={style.cardContainer}>
             <div className={style.info}>
                 <h3>{title}</h3>
                 <p className={style.description}>{description}</p>
-                <a target="_blank" href={link}>Acessar Repositório</a>
+                <a target="_blank" href={linkRepo}>Acessar Repositório</a>
+                <a target="_blank" href={linkPage}>Acessar Página</a>
             </div>
             <div className={style.projectImage}>
                 <img src={src} alt={alt} />
--- src/components/ProjectsSection/index.jsx
+++ src/components/ProjectsSection/index.jsx
@@ -16,19 +16,22 @@ export const Projects = () => {
             <section className={style.cardsSection}>
                 <ProjectsCard title={"Driming"} 
                 description={"Projeto pessoal que desenvolvi inteiramente usando ReactJs e Sass, trata-se de um site de streaming chamado 'Driming' que é uma marca pessoal, e possui uma seção de animes e uma de filmes, contando também com um cabeçalho com um menu de navegação e introduz um filme, e um rodapé."}
-                link={"https://github.com/Adrian-Costa/Driming"}
+                linkRepo={"https://github.com/Adrian-Costa/Driming"}
+                linkPage={"https://adrian-costa.github.io/Driming/"}
                 src={Driming}
                 alt={"Driming project image"} />
 
                 <ProjectsCard title={"Maratona Invertida"} 
                 description={"Projeto de estudos feito durante meu curso, desenvolvi usando html e css, representa um site de streaming inspirado em Stranger Things, nele foram trabalhados principalmente os conceitos de MediaQuery e Responsividade."}
-                link={"https://github.com/Adrian-Costa/projeto-st"}
+                linkRepo={"https://github.com/Adrian-Costa/projeto-st"}
+                linkPage={"https://adrian-costa.github.io/projeto-st/"}
                 src={Maratona}
                 alt={"Driming project image"} />
 
                 <ProjectsCard title={"Verificador de idade"} 
                 description={"Um projeto que fiz antes de iniciar meu curso, que trabalha principalmente com lógica de programação, foi utilizado, Javascript, Html, e CSS. O ano de nascimento é inserido e a idade é calculada subtraindo o ano de nascimento do ano atual, e após uma série de confirmações é impresso na tela uma imagem e mensagem que represente a idade obtida como resultado."}
-                link={"https://github.com/Adrian-Costa/Verificador-de-Idade"}
+                linkRepo={"https://github.com/Adrian-Costa/Verificador-de-Idade"}
+                linkPage={"https://adrian-costa.github.io/Verificador-de-Idade/"}
                 src={Verificador}
                 alt={"Driming project image"} />
             </section>
